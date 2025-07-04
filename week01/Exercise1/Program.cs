7855384e16673c9a554c349c077a029cf592cca3
AcXirus <dtorresflor@byupathway.edu>
2025-07-04 11:11:07 -0500

    finish exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        string fristname = Console.ReadLine();
+        Console.Write("what is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {fristname}{lastname}");
     }
 }
