00b588395ea8f325e663d2b76843901bc3f5ef13
AcXirus <dtorresflor@byupathway.edu>
2025-07-04 17:18:12 -0500

    finish exercise 3

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("This is the Exercise4 Project.");
+        //CREATE A NEW LIST
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        //ADD THE WHILE LOOP 
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        //SUM THE NUMBER INSIDE THE LIST
+        int sum = 0;
+        foreach (int numberList in numbers)
+        {
+            sum += numberList;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //CALCULATE THE AVERGE 
+        float Average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {Average}");
+
+        //FIND THE MAX 
+        int max = numbers[0];
+        foreach (int numberList in numbers)
+        {
+            if (numberList > max)
+            {
+                max = numberList;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
