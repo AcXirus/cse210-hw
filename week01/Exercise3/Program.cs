cda2b9fe22d63a964deb136bfe27b7b4e9a7d6ea
AcXirus <dtorresflor@byupathway.edu>
2025-07-04 14:43:59 -0500

    finish exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        Console.WriteLine("This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is the guess number? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+
+            if (guessNumber == magicNumber)
+            {
+                Console.Write("You guessed it");
+            }
+            else if (guessNumber >= magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber <= magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.Write("Number not found");
+            }
+        }
+   }
 }
