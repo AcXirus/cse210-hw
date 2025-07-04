6c9df9e4fd9fa90afcd1d8a5f2747c216733bdd1
AcXirus <dtorresflor@byupathway.edu>
2025-07-04 11:16:57 -0500

    Finish exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -10,6 +10,6 @@ class Program
         string fristname = Console.ReadLine();
         Console.Write("what is your last name? ");
         string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {fristname}{lastname}");
+        Console.WriteLine($"Your name is {lastname}, {fristname} {lastname}");
     }
 }
