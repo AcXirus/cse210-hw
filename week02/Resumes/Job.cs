811bc50d7cafa5e6d6c6f997d9d8f73b9a43b344
AcXirus <dtorresflor@byupathway.edu>
2025-07-09 14:06:00 -0500

    Finish activity abstraction Week 02

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    public string _JobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_JobTitle} ({_company}) {_startYear} {_endYear}");
+    }
+}
+
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._JobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2025;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+
+        job2._JobTitle = "Front End Developer";
+        job2._company = "SpaceX";
+        job2._startYear = 2025;
+        job2._endYear = 2030;
+
+        Resume Myself = new Resume();
+        Myself._name = "Dillan S Torres";
+        Myself._jobs.Add(job1);
+        Myself._jobs.Add(job2);
+
+        Myself.DisplayResume();
     }
 }
